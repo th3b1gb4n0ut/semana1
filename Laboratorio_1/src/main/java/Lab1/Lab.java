ad18968ce5dce5478344b81ecbad359eb416ab78
Wilfran Isai Lazaro Barrientos <41808497+th3b1gb4n0ut@users.noreply.github.com>
2022-05-22 05:59:17 -0500

    Add files via upload

---
+++ Laboratorio_1/src/main/java/Lab1/Lab.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class Lab {
+    
+    
+    static Scanner reader = new Scanner(System.in);
+       
+       public static void llenado(){
+           
+           System.out.println("Ingrese el tamaño de todos los lados del triangulo");
+            
+          int lee = reader.nextInt();
+          
+           
+       }
+        
+    
+    public String r;
+    
+    public String triangulo(int lado1, int lado2, int lado3 ){
+        
+        
+        
+        
+        if( lado1 == lado2 && lado2 == lado3){
+            
+            r = "hola";
+            
+            return r;
+            
+        }
+        return null;
+        
+            
+} 
+        
+        
+       
+       
+        
+    
+    }
+    
+       
+               
+     
---
+++ Laboratorio_1/src/main/java/Lab1/LabRun.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab1;
+import java.util.Scanner;
+/**
+ *
+ * @author user
+ */
+public class LabRun extends Lab{
+    
+    public static void main(String[] args){
+        
+      
+        
+        
+    }
+    
+}
